73e1b1b2c963014ff621eb8f27401f4eec919fe2
Rodrigo Castro Daflon <rcdaflon@outlook.com>
2021-11-10 09:55:54 -0300

    remove duplcated code

--- src/presentation/routers/loginRouter.test.js
+++ src/presentation/routers/loginRouter.test.js
@@ -7,7 +7,8 @@ const generate = function () {
 
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
