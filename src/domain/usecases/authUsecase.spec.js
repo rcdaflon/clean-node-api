1756a3b7e931a77b6e84b0c231a67f6d773dffe3
Rodrigo Castro Daflon <rcdaflon@outlook.com>
2021-11-11 16:21:19 -0300

    ensure AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/authUsecase.spec.js
+++ src/domain/usecases/authUsecase.spec.js
@@ -19,7 +19,10 @@ class AuthUseCase {
     if (!this.loadUserByEmailRepository.load) {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -67,5 +70,11 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth(generateParam(), generateParam())
     expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(generateParam(), generateParam())
+    expect(accessToken).toBeNull()
   })
 })
